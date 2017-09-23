e9083b162e6755271e13bf676958b0252c7e9ee5
sdyy1990 <ye.yu@uky.edu>
2017-09-23 01:41:12 -0400

    build on uky dlx

---
+++ buildUKYDLX.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+module add cmake/2.8.4
+module add gcc/4.9.1
+sed '/test/d' -i CMakeLists.txt
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER='g++'
+make -j4
--- runUKYDLX.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-module add cmake/2.8.4
-module add gcc/4.9.1
-
